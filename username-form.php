8ee54de9986a23b9f6dad52dfbd107bd659627c3
David Mansaray <david@davidmansaray.com>
2019-02-15 23:48:57 -0300

    Username form

---
+++ username-form.php
@@ -0,0 +1,11 @@
+<br>
+
+<form method="POST">
+    <label for="torre_username">Torre.co Username</label>
+    <input type="text" name="torre_username" id="torre_username" value="<?php echo $value; ?>">
+    <input type="submit" value="Save" class="button button-primary button-large">
+</form>
+
+<br>
+
+Don't have an account on torre.co? <a href="https://accounts.torre.co/email/"> Sign up! </a>
